c6f94d2b648249a7c798175b7ca5df971ba7a507
Mirjalol Bakhtiyorovic <140687033+max97268@users.noreply.github.com>
2024-07-09 19:27:41 +0500

    Add files via upload

---
+++ pages/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+
+     path('', Home, name='home'),
+    # path('brands/', Brands, name='brands'),
+    # path('contact/', Contact, name='contact'),
+    # path('featured_cars/', Featured_Cars, name='feature_cars'),
+    # path('new_cars/', New_Cars, name='new_cars'),
+    # path('service/', Service, name='service'),
+
+]
+
