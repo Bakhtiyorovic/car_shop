cf3620e0688e366aa441bf2701d6d26ba05aa535
cyper_wolf <140687033+max97268@users.noreply.github.com>
2024-07-29 10:18:51 +0500

    Fayllar yangilandi

--- pages/models.py
+++ pages/models.py
@@ -9,3 +9,13 @@ class Home_page(models.Model):
     
     def __str__(self):
         return self.title
+    
+
+
+class Contact_page(models.Model):
+    description = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.email
--- pages/views.py
+++ pages/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import *
 # Create your views here.
+
 def Home(request):
     home_entry = Home_page.objects.all()
     return render(request, 'index.html', {'home_entry': home_entry})
 
 def Contact(request):
-    return render(request, 'contact.html')
+    contact_data = Contact_page.objects.all()
+    return render(request, 'contact.html', {'contact_data': contact_data})
 
 def Service(request):
     return render(request, 'service.html')
